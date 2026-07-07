ccb743c8a13804fc71dec63a7e2d18e6ed0390b2
AceAsket <aceasket@users.noreply.github.com>
2026-07-07 23:33:26 +0300

    Use direct links for book downloads

--- client/components/Search/BaseList.js
+++ client/components/Search/BaseList.js
@@ -220,6 +220,15 @@ export default class BaseList {
         }, 1000);
     }
 
+    downloadHref(href) {
+        const link = document.createElement('a');
+        link.href = href;
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+
     async getErrorMessage(error) {
         if (error.response && error.response.data) {
             const responseData = error.response.data;
@@ -296,9 +305,13 @@ export default class BaseList {
 
             //action
             if (action == 'download') {
-                const downloadResponse = await axios.get(href, {responseType: 'blob'});
-                const fileName = this.getDownloadFileName(downloadResponse, book, format);
-                this.downloadBlob(downloadResponse.data, fileName);
+                if (format) {
+                    const downloadResponse = await axios.get(href, {responseType: 'blob'});
+                    const fileName = this.getDownloadFileName(downloadResponse, book, format);
+                    this.downloadBlob(downloadResponse.data, fileName);
+                } else {
+                    this.downloadHref(href);
+                }
             } else if (action == 'copyLink') {
                 //копирование ссылки
                 if (await utils.copyTextToClipboard(href))
